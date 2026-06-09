4bfb0274fea65da7fde161edc58debedaa82c71c
Emily Stein <emilystein@Emilys-Mac-mini.local>
2026-06-09 06:40:02 -0600

    Initial Commit

---
+++ BedrockResearchTests/BedrockResearchTests.swift
@@ -0,0 +1,18 @@
+//
+//  BedrockResearchTests.swift
+//  BedrockResearchTests
+//
+//  Created by Emily Stein on 6/9/26.
+//
+
+import Testing
+
+struct BedrockResearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
